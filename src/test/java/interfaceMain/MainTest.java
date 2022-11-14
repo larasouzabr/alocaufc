f832b76a92a208915aa49ec1fcbbe9147de77fe9
Vinícius Lemos <vinicius.error@gmail.com>
2022-11-14 16:03:56 -0300

    added test in main interface

--- src/test/java/interfaceMain/MainTest.java
+++ src/test/java/interfaceMain/MainTest.java
@@ -1,4 +1,50 @@
+package interfaceMain;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseButton;
+import javafx.stage.Stage;
+import org.jetbrains.annotations.NotNull;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.loadui.testfx.GuiTest;
+import org.testfx.api.FxToolkit;
+import org.testfx.framework.junit.ApplicationTest;
+
+import java.io.IOException;
+
 import static org.junit.Assert.*;
-public class MainTest {
-  
+import static org.testfx.api.FxAssert.verifyThat;
+import static org.testfx.matcher.base.WindowMatchers.isShowing;
+
+public class MainTest extends ApplicationTest {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Main.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 844, 504);
+        stage.setTitle("Aloca UFC");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        FxToolkit.hideStage();
+        release(new KeyCode[]{});
+        release(new MouseButton[]{});
+    }
+
+    @Test
+    public void testStartInterface() {
+        verifyThat(window("Aloca UFC"), isShowing());
+    }
 }
